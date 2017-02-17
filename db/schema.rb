ecc6a730258d2a44834b498a5d158057d8b753f3
Jason Amerine <amerine.19@gmail.com>
2017-02-17 09:53:54 -0500

    postgres change

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206154707) do
+ActiveRecord::Schema.define(version: 20170118191245) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
     t.string   "business_name",               null: false
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
     t.integer  "industry_type",   default: 0
     t.integer  "total_employees", default: 0
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
   create_table "leads", force: :cascade do |t|
@@ -51,9 +54,10 @@ ActiveRecord::Schema.define(version: 20170206154707) do
     t.integer  "account_id"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.index ["account_id"], name: "index_users_on_account_id"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["account_id"], name: "index_users_on_account_id", using: :btree
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "users", "accounts"
 end
